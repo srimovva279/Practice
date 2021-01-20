104f3083749d3b8b5554605fda328ffcc885dd00
srimovva279 <sri.movva279@gmail.com>
2021-01-20 21:57:39 +0530

    ok

---
+++ src/org/practice/Sample1.java
@@ -0,0 +1,20 @@
+package org.practice;
+
+public class Sample1 {
+
+	public void add() {
+		
+		System.out.println("20+30");
+
+	}
+	public void sub() {
+		System.out.println("30-20");
+
+	}
+	
+	public static void main(String[] args) {
+		Sample1 s=new Sample1();
+		s.add();
+		s.sub();
+	}
+}
