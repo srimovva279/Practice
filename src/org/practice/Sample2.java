3ad60880377b0bf5272ce26c4e604e965700ff0a
srimovva279 <sri.movva279@gmail.com>
2021-01-20 22:04:27 +0530

    ok

---
+++ src/org/practice/Sample2.java
@@ -0,0 +1,13 @@
+package org.practice;
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("praveen");
+		System.out.println("Srikanth");
+		System.out.println("jagadeesh");
+
+	}
+
+}
